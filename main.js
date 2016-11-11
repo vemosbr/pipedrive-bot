7ed7c26d186ca05f4e98c74d36a45d60923f97ab
Mehfuz Hossain <mehfuz@gmail.com>
2016-11-10 23:23:07 -0800

    Fixes

--- main.js
+++ main.js
@@ -2,6 +2,43 @@
 var util = require('util');
 var PipeDrive = require('pipedrive');
 var request = require('request');
+var Status = (function () {
+    function Status() {
+    }
+    Object.defineProperty(Status, "OPEN", {
+        get: function () { return "open"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Status, "DELETED", {
+        get: function () { return "deleted"; },
+        enumerable: true,
+        configurable: true
+    });
+    return Status;
+}());
+exports.Status = Status;
+var Event = (function () {
+    function Event() {
+    }
+    Object.defineProperty(Event, "ADDED_DEAL", {
+        get: function () { return "added.deal"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Event, "ADDED_NOTE", {
+        get: function () { return "added.note"; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Event, "UPDATED_DEAL", {
+        get: function () { return "updated.note"; },
+        enumerable: true,
+        configurable: true
+    });
+    return Event;
+}());
+exports.Event = Event;
 var Bot = (function () {
     function Bot(args) {
         if (typeof this._api_token === 'undefined') {
@@ -38,36 +75,37 @@ var Bot = (function () {
         var event = this.args.event;
         if (that.events.indexOf(event) !== -1) {
             var host_1 = this.args.meta.host;
-            var id_1 = that.getDealId(event, that.args.current);
             var userId = that.args.meta.user_id;
             that.pipeClient.Users.get(userId, function (err, user) {
                 if (err) {
                     throw err;
                 }
-                if (user && id_1) {
-                    that.pipeClient.Deals.get(id_1, function (err, deal) {
-                        if (err) {
-                            throw err;
-                        }
+                if (user) {
+                    that.getDeal(that.args, function (deal) {
                         if (deal && deal.id) {
                             var deal_site_url_1 = util.format("https://%s/deal/%s", host_1, deal.id);
                             var body = {
                                 "username": "pipedrive",
                                 "icon_url": "https://www.recime.ai/image/pipedrive"
                             };
-                            if (deal.status === "open") {
-                                that.pipeClient.Stages.get(deal.stage_id, function (err, stage) {
-                                    if (event === "added.deal") {
-                                        body['text'] = util.format("<mailto:%s|%s> has added a deal *%s*", user.email, user.name, deal.title);
-                                    }
-                                    else if (event === "added.note") {
-                                        body['text'] = util.format("<mailto:%s|%s> has added note in deal *%s* \n%s", user.email, user.name, deal.title, deal_site_url_1);
-                                    }
-                                    else {
-                                        body['text'] = util.format("<mailto:%s|%s> has updated the deal *%s* to `%s` \n%s", user.email, user.name, deal.title, stage.name, deal_site_url_1);
+                            if (deal.status === Status.OPEN && deal.stage_id) {
+                                if (event === Event.ADDED_DEAL || event === Event.ADDED_NOTE) {
+                                    body['text'] = util.format("<mailto:%s|%s> has %s in *%s* \n%s", user.email, user.name, event.split(".").join(" a "), deal.title, deal_site_url_1);
+                                    if (event === Event.ADDED_DEAL) {
+                                        body['text'] = util.format("<mailto:%s|%s> has added a new deal *%s* \n%s", user.email, user.name, deal.title, deal_site_url_1);
                                     }
                                     that.postToSlack(body, cb);
-                                });
+                                }
+                                else {
+                                    // stage_order_nr
+                                    that.pipeClient.Stages.get(deal.stage_id, function (err, stage) {
+                                        if (err) {
+                                            throw err;
+                                        }
+                                        body['text'] = util.format("<mailto:%s|%s> has updated the deal *%s* to `%s` \n%s", user.email, user.name, deal.title, stage.name, deal_site_url_1);
+                                        that.postToSlack(body, cb);
+                                    });
+                                }
                             }
                             else {
                                 var status_1 = deal.status.substr(0, 1).toUpperCase() + deal.status.substr(1, deal.status.length);
@@ -89,20 +127,28 @@ var Bot = (function () {
             throw util.format("Event: %s not supported", event);
         }
     };
-    Bot.prototype.getDealId = function (event, current) {
-        var _id = null;
-        if (event === "added.note") {
-            _id = this.args.current.deal_id;
+    Bot.prototype.getDeal = function (args, cb) {
+        var event = args.event;
+        if (event === Event.ADDED_NOTE) {
+            var dealId = args.current.deal_id;
+            this.pipeClient.Deals.get(dealId, function (err, deal) {
+                if (err) {
+                    throw err;
+                }
+                cb(deal);
+            });
         }
         else {
-            if (this.args.meta.action === "deleted") {
-                _id = this.args.previous.id;
+            if (this.args.meta.action === Status.DELETED) {
+                return cb(args.previous);
             }
-            else if (this.args.current) {
-                _id = this.args.current.id;
+            else if ((event === Event.ADDED_DEAL)
+                || (args.current && args.current.stage_id !== args.previous.stage_id)
+                || (args.current.status !== Status.OPEN)) {
+                return cb(args.current);
             }
+            cb(null);
         }
-        return _id;
     };
     Bot.prototype.postToSlack = function (body, cb) {
         var that = this;
--- main.ts
+++ main.ts
@@ -2,6 +2,18 @@ import util = require('util')
 import PipeDrive = require('pipedrive');
 import request = require('request');
 
+
+export class Status {
+    public static get OPEN():string { return "open"; }
+    public static get DELETED():string { return "deleted"; }
+}
+
+export class Event {
+    public static get ADDED_DEAL():string { return "added.deal"; }
+    public static get ADDED_NOTE():string { return "added.note"; }
+    public static get UPDATED_DEAL():string { return "updated.note"; }
+}
+
 export class Bot {
 
   private args: any;
@@ -42,18 +54,14 @@ export class Bot {
 
     if (that.events.indexOf(event) !== -1){
        let host = this.args.meta.host;
-       let id = that.getDealId(event, that.args.current);
        let userId = that.args.meta.user_id;
 
        that.pipeClient.Users.get(userId, (err, user)=>{
          if (err){
             throw err
          }
-         if (user && id){
-           that.pipeClient.Deals.get(id, function(err, deal){
-               if (err){
-                  throw err 
-               }
+         if (user){
+           that.getDeal(that.args, (deal)=>{
                if (deal && deal.id){
                  let deal_site_url = util.format("https://%s/deal/%s", host, deal.id);
 
@@ -62,20 +70,26 @@ export class Bot {
                    "icon_url" : "https://www.recime.ai/image/pipedrive"
                  };
 
-                 if (deal.status === "open"){
-                   that.pipeClient.Stages.get(deal.stage_id, function(err, stage){
-                     if (event === "added.deal"){
-                       body['text'] = util.format("<mailto:%s|%s> has added a deal *%s*", user.email, user.name, deal.title);
-                     }
-                     else if (event === "added.note"){
-                       body['text'] = util.format("<mailto:%s|%s> has added note in deal *%s* \n%s", user.email, user.name, deal.title, deal_site_url);
-                     }
-                     else {
-                       body['text'] = util.format("<mailto:%s|%s> has updated the deal *%s* to `%s` \n%s", user.email,  user.name, deal.title, stage.name, deal_site_url);
-                     }
-
-                     that.postToSlack(body, cb);
-                   });
+                 if (deal.status === Status.OPEN && deal.stage_id){
+                    if (event === Event.ADDED_DEAL || event === Event.ADDED_NOTE){
+                      body['text'] = util.format("<mailto:%s|%s> has %s in *%s* \n%s", user.email, user.name, event.split(".").join(" a "), deal.title, deal_site_url);
+                    
+                      if (event === Event.ADDED_DEAL){
+                         body['text'] = util.format("<mailto:%s|%s> has added a new deal *%s* \n%s", user.email, user.name, deal.title, deal_site_url);
+                      }
+                      that.postToSlack(body, cb);
+                    }
+                    else {
+                      // stage_order_nr
+                      that.pipeClient.Stages.get(deal.stage_id, (err, stage)=>{
+                        if (err){
+                          throw err;
+                        }
+                        body['text'] = util.format("<mailto:%s|%s> has updated the deal *%s* to `%s` \n%s", user.email,  user.name, deal.title, stage.name, deal_site_url);
+                       
+                        that.postToSlack(body, cb);
+                      });
+                    }
                  }
                  else {
                    let status = deal.status.substr(0, 1).toUpperCase() + deal.status.substr(1, deal.status.length);
@@ -97,21 +111,29 @@ export class Bot {
     }
   }
 
-  private getDealId(event:string, current:any) {
-    var _id = null;
+  private getDeal(args:any, cb:any) {
+    let event = args.event;
 
-    if (event === "added.note"){
-       _id = this.args.current.deal_id;
+    if (event === Event.ADDED_NOTE){
+       let dealId = args.current.deal_id;
+       this.pipeClient.Deals.get(dealId, (err, deal)=>{
+            if (err) {
+                throw err;
+            }
+            cb(deal);
+       });
     }
     else {
-      if (this.args.meta.action === "deleted"){
-         _id = this.args.previous.id;
+      if (this.args.meta.action === Status.DELETED){
+         return cb(args.previous);
       }
-      else if (this.args.current){
-        _id =  this.args.current.id;
+      else if ((event === Event.ADDED_DEAL) 
+      || (args.current && args.current.stage_id !== args.previous.stage_id)
+      || (args.current.status !== Status.OPEN)){
+        return cb(args.current);
       }
+      cb(null);
     }
-    return _id;
   }
 
   private postToSlack(body:any, cb: any){
