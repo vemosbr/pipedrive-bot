f0ac160739287202dee9cd517439de8db456e607
Marcio Lima <mdlima.github@mailnull.com>
2017-01-23 22:08:27 -0200

    Add more details as attachments to Slack messages.

--- main.ts
+++ main.ts
@@ -9,10 +9,17 @@ export class Status {
     public static get DELETED():string { return "deleted"; }
 }
 
-export class Event {
-    public static get ADDED_DEAL():string { return "added.deal"; }
-    public static get ADDED_NOTE():string { return "added.note"; }
-    public static get UPDATED_DEAL():string { return "updated.note"; }
+export class PipedriveObject {
+    public static get ACTIVITY():string { return "activity"; }
+    public static get DEAL():string { return "deal"; }
+    public static get NOTE():string { return "note"; }
+}
+
+export class EventType {
+    public static get ADDED():string { return "added"; }
+    public static get UPDATED():string { return "updated"; }
+    public static get DELETED():string { return "deleted"; }
+    public static get MERGED():string { return "merged"; }
 }
 
 export class Bot {
@@ -27,9 +34,12 @@ export class Bot {
     this.pipeClient = new PipeDrive.Client(process.env.API_TOKEN);
 
     this.events = [
+        "added.activity",
+        "updated.activity",
         "added.note",
-        "updated.deal",
+        "updated.note",
         "added.deal",
+        "updated.deal",
         "deleted.deal"
     ];
   }
@@ -37,6 +47,9 @@ export class Bot {
   execute(cb:any){
     let that = this;
     let event = this.args.event;
+    let current = this.args.current;
+    let eventType = event.split(".")[0];
+    let eventObject = event.split(".")[1];
 
     if (that.events.indexOf(event) !== -1){
        let host = this.args.meta.host;
@@ -52,36 +65,52 @@ export class Bot {
                if (deal && deal.id){
                  let deal_site_url = util.format("https://%s/deal/%s", host, deal.id);
 
-                 var body = {
-                   "username" : "pipedrive",
-                   "icon_url" : "https://www.recime.ai/image/pipedrive"
+                 let body = {
+                   "username" : "Pipedrive",
+                   "icon_url" : "https://www.pipedrive.com/images/favicons/apple-touch-icon-72x72.png",
+                   "attachments": [
+                     {
+                        "title": deal.title,
+                        "title_link": deal_site_url,
+                        "mrkdwn_in": ["text", "pretext"]
+                     }
+                   ]
                  };
 
                  if (deal.status === Status.OPEN && deal.stage_id){
-                    if (event === Event.ADDED_DEAL || event === Event.ADDED_NOTE){
-                      body['text'] = util.format("<mailto:%s|%s> has %s in *%s* \n%s", user.email, user.name, event.split(".").join(" a "), deal.title, deal_site_url);
+                    if (eventObject === PipedriveObject.DEAL){
+                      if (eventType === EventType.ADDED){
+                         body['text'] = util.format("<mailto:%s|%s> has added a new deal *%s*", user.email, user.name, deal.title);
+                      }
+                      else {
+                        // stage_order_nr
+                        that.pipeClient.Stages.get(deal.stage_id, (err, stage)=>{
+                          if (err){
+                            throw err;
+                          }
+                          body['text'] = util.format("<mailto:%s|%s> has updated the deal *%s* to `%s`", user.email,  user.name, deal.title, stage.name);
 
-                      if (event === Event.ADDED_DEAL){
-                         body['text'] = util.format("<mailto:%s|%s> has added a new deal *%s* \n%s", user.email, user.name, deal.title, deal_site_url);
+                        });
                       }
                       that.postToSlack(body, cb);
                     }
                     else {
-                      // stage_order_nr
-                      that.pipeClient.Stages.get(deal.stage_id, (err, stage)=>{
-                        if (err){
-                          throw err;
-                        }
-                        body['text'] = util.format("<mailto:%s|%s> has updated the deal *%s* to `%s` \n%s", user.email,  user.name, deal.title, stage.name, deal_site_url);
+                      body['text'] = util.format("<mailto:%s|%s> has %s in *%s*", user.email, user.name, event.split(".").join(" a "), deal.title);
 
-                        that.postToSlack(body, cb);
-                      });
+                      if (eventObject === PipedriveObject.NOTE){
+                        body['attachments'][0]['text'] = current['content'];
+                      }
+
+                      if (eventObject === PipedriveObject.ACTIVITY){
+                        body['attachments'][0]['text'] = util.format("Activity *%s* %s.\n*Notes:* %s", current['subject'], eventType, current['note']);
+                      }
+                      that.postToSlack(body, cb);
                     }
                  }
                  else {
                    let status = deal.status.substr(0, 1).toUpperCase() + deal.status.substr(1, deal.status.length);
 
-                   body['text'] = util.format("<mailto:%s|%s> has changed status of deal *%s* to `%s` \n%s", user.email, user.name, deal.title, status, deal_site_url);
+                   body['text'] = util.format("<mailto:%s|%s> has changed status of deal *%s* to `%s`", user.email, user.name, deal.title, status);
 
                    that.postToSlack(body, cb);
                  }
@@ -102,8 +131,10 @@ export class Bot {
 
   private getDeal(args:any, cb:any) {
     let event = args.event;
+    let eventType = event.split(".")[0];
+    let eventObject = event.split(".")[1];
 
-    if (event === Event.ADDED_NOTE){
+    if (eventObject === PipedriveObject.NOTE || eventObject === PipedriveObject.ACTIVITY){
       let dealId = args.current.deal_id;
       if (dealId){
          this.pipeClient.Deals.get(dealId, (err, deal)=>{
@@ -120,7 +151,7 @@ export class Bot {
       if (this.args.meta.action === Status.DELETED){
          return cb(args.previous);
       }
-      else if ((event === Event.ADDED_DEAL)
+      else if ((eventObject === PipedriveObject.DEAL && eventType === EventType.ADDED)
       || (args.current && args.current.stage_id !== args.previous.stage_id)
       || (args.current.status !== Status.OPEN)){
         return cb(args.current);
